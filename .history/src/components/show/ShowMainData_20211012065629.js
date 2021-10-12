ba378d19761c6dd9dc8de7e3ecabb1ee245a1bd5
Mayur2905 <mayurgaikwad0101@gmail.com>
2021-10-12 07:25:45 +0530

    Styled show page

--- .history/src/components/show/ShowMainData_20211012065629.js
+++ .history/src/components/show/ShowMainData_20211012065629.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import IMG_PLACEHOLDER from '../../images/not-found.png';
 import {Star} from '../styled'
-import { Headline, MainDataWrapper, TagList } from './ShowMainData.styled';
 
 const ShowMainData = ({ name, rating, summary, tags, image }) => {
     return (
-      <MainDataWrapper>
+      <div>
         <img src={image ? image.original : IMG_PLACEHOLDER} alt="show-cover" />
-        <div className="text-side">
-          <Headline>
+        <div>
+          <div>
             <h1>{name}</h1>
             <div>
               <Star />
               <span>{rating.average || 'N/A'}</span>
             </div>
-          </Headline>
-          <div className="summary" dangerouslySetInnerHTML={{ __html: summary }} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: summary }} />
   
           <div>
             Tags:{' '}
-            <TagList>
+            <div>
               {tags.map((tag, i) => (
                 <span key={i}>{tag}</span>
               ))}
-            </TagList>
+            </div>
           </div>
         </div>
-      </MainDataWrapper>
+      </div>
     );
   };
 
